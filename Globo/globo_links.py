ea138026eeae81c41756d6caeab7bc996e80f8c9
Fabricio Colombo <fabriciocolombo0096@gmail.com>
2024-03-15 08:34:07 -0300

    globo
    scraping in globo.com

---
+++ Globo/globo_links.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Accept-Language': 'en-US,en;q=0.9,pt;q=0.8',
+}
+
+url = 'https://www.globo.com/'
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'html.parser')
+    all_links = soup.find_all('a')
+    
+    # Listar todos os href da página
+    for link in all_links:
+        href = link.get('href')
+        if href:
+            print(href)
+else:
+    print('Failed to retrieve data. Status code:', response.status_code)
